a21f57cfd9666173fea3f541c1d11d84b132ced4
evilaughter <62458450+evilaughter@users.noreply.github.com>
2020-11-19 21:17:19 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,14 @@
+def swap():
+    f1=input('enter file name -')
+    f2=input('enter file name -')
+    with open (f1,'r') as a :
+        dataA=a.read()
+    with open (f2,'r') as b :
+        dataB=b.read()
+    with open (f1,'w') as a :
+        a.write(dataB)
+    with open (f2,'w') as b :
+        b.write(dataA)
+
+swap()        
+ 
